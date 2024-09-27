cc1c30a2e20d940f665b7574713f8ad8516ea602
Henzz <khenok53@gmail.com>
2024-09-27 17:27:07 +0300

    task 2 fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
 -- Rank the country origins of metal bands by the number of non-unique fans
-SELECT
-  origin,
-  SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
